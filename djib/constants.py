990f8edd8c048852122fe2d70f79aea84a2e7cbc
Aidin Mohandesi <Aidin.mohandesi@gmail.com>
2022-08-24 03:55:49 +0430

    add constants

--- djib/constants.py
+++ djib/constants.py
@@ -6,6 +6,6 @@ KMS_DEV_ENDPOINT: str = "https://devkms.djib.io"
 KMS_MAIN_ENDPOINT: str = "https://kms.djib.io"
 """ Djib Key Manager Service Main-net Providers """
 
-PEERS_ENDPOINT: str = "https://nodes.djib.io/api/peers/relay"
+PEERS_ENDPOINT: str = "https://nodes.djib.io/api/peers/"
 """ Djib Peers List """
 
